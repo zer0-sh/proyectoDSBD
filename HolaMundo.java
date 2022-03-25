18efb2295e668bea1a7d0c1fc82038935afd7764
zer0sh <58661882+zer0-sh@users.noreply.github.com>
2022-03-24 23:28:20 -0400

    Create HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+	public static void main(String[] args) {		
+        System.out.println("Hola, este es el principio del repositorio jeje");
+        //hola jeje este es un saludo de prueba
+    
+	}
+}
